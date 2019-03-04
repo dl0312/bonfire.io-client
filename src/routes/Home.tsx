2e3305dd12b075bccbf8edc2674a9f857b2a738e
GeonLee <leegun2003@gmail.com>
2019-03-04 17:34:59 +0900

    fix: mobile chrome youtube bug

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -26,7 +26,12 @@ function Home() {
         width="100%"
         height="100%"
         onReady={() => console.log("video ready")}
-        onStart={() => console.log("video start")}
+        onStart={() => {
+          console.log("video start");
+          setTimeout(() => {
+            setVideoStart(true);
+          }, 2000);
+        }}
         onPlay={() => {
           console.log("video play");
           setTimeout(() => {
@@ -40,7 +45,7 @@ function Home() {
           overflow: "hidden",
           position: "absolute",
           zIndex: -1,
-          filter: `blur(2px) brightness(${videoStart ? 0.8 : 0})`
+          filter: `blur(2px) brightness(${videoStart ? 0.8 : 0.8})`
         }}
       />
       {!login ? (
